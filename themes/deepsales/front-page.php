3ded636aacae4ebc38902ba279606e912a611079
Al.exe <al_exe@ukr.net>
2025-11-17 08:46:36 +0200

    Make files project

---
+++ themes/deepsales/front-page.php
@@ -0,0 +1,19 @@
+<?php get_header(); ?>
+    <div class="hero">
+        <div class="wrapper">
+            <div class="hero-lefrt">
+                <h1 class="title-hero">
+                    Перестаньте втрачати угоди через хаосу відділі продажів
+                </h1>
+                <p class="text-hero">
+                    Власникам і керівникам: процеси, метрики, дисципліна в CRM, навички команди й регулярний менеджмент — з прогнозованим результатом
+                </p>
+                <button class="button-hero">Замовити консультацію</button>
+            </div>
+            <div class="hero-right">
+                <img src="" alt="" class="hero-img">
+            </div>
+        </div>
+    </div>
+
+<?php get_footer(); ?>
---
+++ themes/deepsales/functions.php
@@ -0,0 +1,55 @@
+<?php 
+
+add_theme_support( 'title-tag' );
+
+add_theme_support( 'custom-logo' );
+
+add_theme_support('menus');
+
+add_theme_support( 'html5', array( 'search-form' ) );
+
+add_theme_support( 'post-thumbnails' );
+
+add_filter('wpcf7_autop_or_not', '__return_false');
+
+
+function remove_image_sizes( $sizes, $metadata ) {
+    return [];
+}
+
+add_filter( 'intermediate_image_sizes_advanced', 'remove_image_sizes', 10, 2 );
+
+
+function cc_mime_types($mimes) {
+    $mimes['svg'] = 'image/svg+xml';
+    return $mimes;
+}
+
+add_filter('upload_mimes', 'cc_mime_types');
+
+add_filter( 'wp_default_scripts', 'remove_jquery_migrate' );
+
+function remove_jquery_migrate( $scripts ) {
+
+	if ( empty( $scripts->registered['jquery'] ) || is_admin() ) {
+		return;
+	}
+
+	$deps = & $scripts->registered['jquery']->deps;
+
+	$deps = array_diff( $deps, [ 'jquery-migrate' ] );
+}
+
+
+add_action('wp_enqueue_scripts', 'deepsales_scripts');
+
+function deepsales_scripts() {
+    wp_enqueue_style('reset', get_template_directory_uri() . '/assets/css/reset.css');
+    wp_enqueue_style('global-style', get_template_directory_uri() . '/assets/css/global.css', array(), filemtime(get_theme_file_path('/assets/css/style.css')));
+    wp_enqueue_style('main-page-style', get_template_directory_uri() . '/assets/css/main-page.css', array(), filemtime(get_theme_file_path('/assets/css/style.css')));
+    wp_enqueue_style('kurs-page-style', get_template_directory_uri() . '/assets/css/kurs-page.css', array(), filemtime(get_theme_file_path('/assets/css/style.css')));
+    wp_enqueue_style('profile-page-style', get_template_directory_uri() . '/assets/css/profile-page.css', array(), filemtime(get_theme_file_path('/assets/css/style.css')));
+
+    wp_enqueue_script('jquery', array(), false, ['in_footer' => true]);
+    wp_enqueue_script('main', get_template_directory_uri() . '/assets/js/main.js', array('jquery'), false, ['in_footer' => true]);
+}
