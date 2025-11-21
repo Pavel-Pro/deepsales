e9acdcaeb1bf94e0d248501d2a23762b3b4133a7
Al.exe <al_exe@ukr.net>
2025-11-21 10:43:24 +0200

    Add Polylang and destop hero section

--- themes/deepsales/front-page.php
+++ themes/deepsales/front-page.php
@@ -1,19 +1,27 @@
 <?php get_header(); ?>
     <div class="hero">
         <div class="wrapper">
-            <div class="hero-lefrt">
-                <h1 class="title-hero">
-                    Перестаньте втрачати угоди через хаосу відділі продажів
-                </h1>
-                <p class="text-hero">
-                    Власникам і керівникам: процеси, метрики, дисципліна в CRM, навички команди й регулярний менеджмент — з прогнозованим результатом
-                </p>
-                <button class="button-hero">Замовити консультацію</button>
-            </div>
-            <div class="hero-right">
-                <img src="" alt="" class="hero-img">
+            <div class="hero__items">
+                <div class="hero-left">
+                    <h1 class="title-hero">
+                        Перестаньте втрачати угоди через хаосу відділі продажів
+                    </h1>
+                    <p class="text-hero">
+                        Власникам і керівникам: процеси, метрики, дисципліна в CRM, навички команди й регулярний менеджмент — з прогнозованим результатом
+                    </p>
+                    <button class="button-hero">Замовити консультацію</button>
+                </div>
+                <div class="hero-right">
+                    <picture>
+                        <source srcset="" media="(max-width: 1300px;)">
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/hero-photo-main.webp" alt="Євгеній Попсуй" class="hero-img" fetchpriority="high">
+                    </picture>
+                    <div class="hero-elips hero-elips__big"></div>
+                    <div class="hero-elips hero-elips__small"></div>
+                </div>
             </div>
         </div>
     </div>
+    <section class="problem"></section>
 
 <?php get_footer(); ?>
--- themes/deepsales/header.php
+++ themes/deepsales/header.php
@@ -13,8 +13,46 @@
 </head>
 <body class="body">
     <header class="header">
-        <div class="header-wrap">
-        
+        <div class="wrapper">
+            <div class="header-wrap">
+                <a href="/" class="logo-link">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/header-logo.svg" alt="Logo Deepsales" class="header-logo">
+                </a>
+                <nav class="nav">
+                    <ul class="menu">
+                        <?php if ( $menu_items = wp_get_nav_menu_items('Main Menu ua') ) {
+                            foreach ( $menu_items as $key => $menu_item ) { ?>
+                                <li class="menu__item">
+                                    <a href="<?php echo $menu_item->url; ?>" class="menu__link"
+                                    <?php echo $menu_item->xfn ? 'rel=" ' . $menu_item->xfn . '"' : ''; ?>>
+                                        <?php echo $menu_item->title; ?>
+                                    </a>
+                                </li>
+                            <?php }
+                        } ?>
+                    <ul>
+                    <burger class="burger">
+                        <span class="burger__item"></span>
+                        <span class="burger__item"></span>
+                        <span class="burger__item"></span>
+                    </burger>
+                </nav>
+                <div class="header-lan">
+                    <?php pll_the_languages(array('
+                        "dropdown" => "0",
+                        "show_names" => "1",
+                        "show_flags" => "0",
+                        "hide_if_no_translation" => "1"
+                    ')); ?>
+                </div>
+                <div class="phone-wrap">
+                    <svg width="19" height="19" viewBox="0 0 19 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M2.18182 0H6.54545L8.72727 5.45455L6 7.09091C7.16832 9.45985 9.0856 11.3771 11.4545 12.5455L13.0909 9.81818L18.5455 12V16.3636C18.5455 16.9423 18.3156 17.4972 17.9064 17.9064C17.4972 18.3156 16.9423 18.5455 16.3636 18.5455C12.1083 18.2869 8.09471 16.4798 5.08018 13.4653C2.06564 10.4507 0.258599 6.43717 0 2.18182C0 1.60316 0.229869 1.04821 0.63904 0.63904C1.04821 0.229869 1.60316 0 2.18182 0Z" fill="white"/>
+                    </svg>
+                    <a href="tel:380932855827" class="header-phone__link">+38 (093) 285-58-27</a>
+                    <button class="header__button">Мій кабінет</button>
+                </div>
+            </div>
         </div>
     </header>
     <main class="main">
