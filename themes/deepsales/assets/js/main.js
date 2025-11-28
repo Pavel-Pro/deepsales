909cf63ae2f78bf28bd20df98b127d57da8b51ad
Al.exe <al_exe@ukr.net>
2025-11-28 19:04:25 +0200

    Finish header and hero desktop

--- themes/deepsales/assets/js/main.js
+++ themes/deepsales/assets/js/main.js
@@ -1,1 +1,29 @@
-    
+    (function($) {
+
+        'use strict';
+
+        $('.burger').on('click', function() {
+            $('.burger__item:first-child').toggleClass('burger__item_top');
+            $('.burger__item:nth-child(2n)').toggleClass('burger__item_middle');
+            $('.burger__item:last-child').toggleClass('burger__item_bottom');
+            $('.menu').toggleClass('menu-active');
+            $('.menu__item, .menu__link, .phone-wrap').toggleClass('show');
+        })
+
+
+        function resize1024() {
+            if ( $(window).width() <= 1024 ) {
+                let contPhone = $('.phone-wrap').html();
+                $('.phone-wrap').appendTo('.menu');
+                $('.menu').find('div').replaceWith(`<li class="phone-wrap">${contPhone}</li>`);
+            }
+            
+        }
+
+        resize1024();
+
+        $(window).on('resize', function() {
+            resize1024();
+        });
+
+    }(jQuery));
--- themes/deepsales/front-page.php
+++ themes/deepsales/front-page.php
@@ -13,7 +13,7 @@
                 </div>
                 <div class="hero-right">
                     <picture>
-                        <source srcset="" media="(max-width: 1300px;)">
+                        <source srcset="<?php echo get_template_directory_uri(); ?>/assets/img/hero-photo-main-tab.webp" media="(max-width: 1024px)">
                         <img src="<?php echo get_template_directory_uri(); ?>/assets/img/hero-photo-main.webp" alt="Євгеній Попсуй" class="hero-img" fetchpriority="high">
                     </picture>
                     <div class="hero-elips hero-elips__big"></div>
@@ -22,6 +22,53 @@
             </div>
         </div>
     </div>
-    <section class="problem"></section>
+    <section class="problem">
+        <div class="wrapper">
+            <h1 class="title title__problem">Проблеми відділу продажу</h1>
+            <span class="subtitle subtitle__problem">Ці проблеми коштують вам мільйони гривень щороку</span>
+            <div class="problem-lists">
+                <div class="problem-item">
+                    <h3 class="title-list">Власники страждають від:</h3>
+                    <ul class="problem__list">
+                        <li class="problem-list__item">
+                            План "скаче" — немає прогнозованих доходів
+                        </li>
+                        <li class="problem-list__item">
+                            Ліди губляться або довго чекають відповіді
+                        </li>
+                        <li class="problem-list__item">
+                            Залежність від ключових менеджерів
+                        </li>
+                        <li class="problem-list__item">
+                            Команда перевтомлена "пожежами"
+                        </li>
+                        <li class="problem-list__item">
+                            Рішення приймаються " на відчуттях"
+                        </li>
+                    </ul>
+                </div>
+                <div class="problem-item">
+                    <h3 class="title-list">Керівники відділів мучаються з:</h3>
+                    <ul class="problem__list">
+                        <li class="problem-list__item">
+                            Менеджери чекають відповіді замість дій
+                        </li>
+                        <li class="problem-list__item">
+                            CRM заповнений хаотично — аналітика неможлива
+                        </li>
+                        <li class="problem-list__item">
+                            Розбори затягуються без конкретних рішень
+                        </li>
+                        <li class="problem-list__item">
+                            Новачки місяцями не можуть стати продуктивними
+                        </li>
+                        <li class="problem-list__item">
+                            Угоди "перескакують" етапи без логіки
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
 
 <?php get_footer(); ?>
