e0ffe8b1aec359378bbb85cc50b5925aa77c880a
Al.exe <al_exe@ukr.net>
2025-12-01 22:37:31 +0200

    finish section effect

--- themes/deepsales/front-page.php
+++ themes/deepsales/front-page.php
@@ -181,7 +181,8 @@
         <div class="wrapper">
             <h2 class="title title__effect">Що отримаєте після впровадження</h2>
             <div class="effect__container">
-                <div class="effect__items">
+                <h4 class="effect__items_title">Для бізнесу:</h4>
+                <div class="effect__items effect__business">
                     <div class="effect__item">
                         <h5 class="effect__item_title">Прогнозований дохід</h5>
                         <p class="effect__item_text">план перестане "скакати"</p>
@@ -191,7 +192,6 @@
                         <p class="effect__item_text">процеси ведуть угоди самі</p>
                     </div>
                     <div class="effect__item">
-                        <h4 class="effect__items_title">Для бізнесу:</h4>
                         <h5 class="effect__item_title">Швидший онбординг</h5>
                         <p class="effect__item_text">нові менеджери готові за 2-3 тижні</p>
                     </div>
@@ -204,7 +204,8 @@
                         <p class="effect__item_text">кожна угода має "наступний крок"</p>
                     </div>
                 </div>
-                <div class="effect__items">
+                <h4 class="effect__items_title">Для команди:</h4>
+                <div class="effect__items effect__team">
                     <div class="effect__item">
                         <h5 class="effect__item_title">Стандартизовані процеси</h5>
                         <p class="effect__item_text">воронка з чіткими правилами</p>
@@ -214,7 +215,6 @@
                         <p class="effect__item_text">скрипти, шаблони, чек-листи</p>
                     </div>
                     <div class="effect__item">
-                        <h4 class="effect__items_title">Для команди:</h4>
                         <h5 class="effect__item_title">Короткі розбори</h5>
                         <p class="effect__item_text">30-45 хв замість 2-3 годин</p>
                     </div>
