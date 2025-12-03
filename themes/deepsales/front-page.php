a93c944e9b4c8f36fd7e09434cf85b8904076a24
Al.exe <al_exe@ukr.net>
2025-12-03 20:25:43 +0200

    finish section effect

--- themes/deepsales/front-page.php
+++ themes/deepsales/front-page.php
@@ -230,6 +230,61 @@
             </div>
              <button class="button effect__button">Обговорити мій кейс</button>
         </div>
+        <div class="steps">
+            <div class="step"></div>
+            <div class="step"></div>
+            <div class="step"></div>
+            <div class="step"></div>
+            <div class="step"></div>
+            <div class="step"></div>
+            <div class="step"></div>
+            <div class="step"></div>
+            <div class="step"></div>
+            <div class="step"></div>
+        </div>
+    </section>
+    <section class="about">
+        <div class="wrapper">
+            <div class="about__top">
+                <h2 class="title title__about">Євгеній Попсуй</h2>
+                <p class="about__top__text">
+                    7+ років у B2B/B2C: Керував відділами в fintech, IT, виробництві; Будує систем продажів з нуля; Навчив 100+ менеджерів системній роботі
+                </p>
+            </div>
+            <div class="about__items">
+                <div class="about__item">
+                    <h4 class="about__item_title">Мій підхід:</h4>
+                    <ul class="about-list">
+                        <li class="about-list__item">
+                            Customer Interview → ICP/Buyer persona → процеси 
+                        </li>
+                        <li class="about-list__item">
+                            Від хаосу до прогнозованих результатів
+                        </li>
+                        <li class="about-list__item">
+                            Мінімум теорії, максимум практики
+                        </li>
+                    </ul>
+                </div>
+                <div class="about__item">
+                    <h4 class="about__item_title">Експертиза:</h4>
+                    <ul class="about-list">
+                        <li class="about-list__item">
+                            Побудова відділів продажів
+                        </li>
+                        <li class="about-list__item">
+                            Побудова відділів продажів
+                        </li>
+                        <li class="about-list__item">
+                            Навчання та онбординг команд
+                        </li>
+                        <li class="about-list__item">
+                            B2B і B2C сегменти
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
     </section>
 
 <?php get_footer(); ?>
